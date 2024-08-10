b2ff38d7c9a557d225f8a48cfdc54ff1e4f61a71
Squire <158856931+SquireCoding@users.noreply.github.com>
2024-08-10 15:22:07 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 var using = '';
 var craften = '';
 var tscore = 0;
+var business = false;
+var modified = 0;
+var transferPerson = 'YBG';
 
 document.getElementsByClassName('errorButton')[0].style.visibility='hidden';
 
@@ -68,15 +71,34 @@ function passTo(username,pass) {
 
 function loadBankPage() {
     tscore = getTScore();
+    modified=0;
     document.body.innerHTML=`
     <h1> Welcome, ${using}</h1> <div class="topcorner" class="logout"><button class="btn" onclick="logout()">Log Out</button></div>
     <hr>
-    <h2> Current T Score (Last Updated 8/8/2024): ${tscore}
+    <h2> Current T Score (Last Updated 8/8/2024): ${tscore}</h2>
     <br>
-    <h2> Current Craften in Account: ${getCraften()}
+    <h2 id="craftenCount"> Current Craften in Account: ${getCraften()}</h2>
     <br>
-    <h2> Cards: ${getCards()}
+    <h2> Cards: ${getCards()}</h2>
+    <br>
+    <button class="wider" onclick="loadTransferPage()">Request Craften Transfer</button>
     `;
+    if (getPurchases()!=null) {
+        document.body.innerHTML+='<br><h2 class="good">Account History:</h2><br><h3> - Transaction benefactor; Amount; Balance After Transaction</h3>'
+        let purs = getPurchases();
+        let actions = [''];
+        for (let i = 0; i<purs.length; i++) {
+            if (purs[i].dep) modified+=purs[i].getValue(); else modified-=purs[i].getValue();
+            actions.push(purs[i].getHtmlRep(modified));
+        }
+        actions.reverse();
+        for (let i = 0; i<purs.length; i++) {
+            document.body.innerHTML+=actions[i];
+        }
+        document.getElementById('craftenCount').innerHTML=` Current Craften in Account: ${modified}`;
+    } else {
+        document.body.innerHTML+='<br><h3 class="green">You have no recorded purchases.</h3>'
+    }
 }
 
 function logout() {
@@ -123,16 +145,16 @@ function getName(user, pass) {
     return null;
 }
 function getTScore() {
-    if (using==='YBG') return 15;
-    if (using==='Cap') return 6;
-    if (using==='Carrie') return 11;
+    if (using==='YBG') return 16;
+    if (using==='Cap') return 5;
+    if (using==='Carrie') return 12;
     if (using==='SMARTS') return 12;
-    if (using==='[E]') return 6;
-    if (using==='Bad Guy') return 10;
+    if (using==='[E]') return 5;
+    if (using==='Bad Guy') return 9;
     if (using==='Deatheye') return 10;
-    if (using==='MortalTripod') return 12;
-    if (using==='SpeedyBek') return 11;
-    if (using==='SparklePony') return 16;
+    if (using==='MortalTripod') return 11;
+    if (using==='SpeedyBek') return 10;
+    if (using==='SparklePony') return 17;
     if (using==='Squire') return 21;
     return "Error getting T score";
 }
@@ -140,26 +162,42 @@ function getCraften() {
     if (using==='YBG') return "83";
     if (using==='Cap') return "1";
     if (using==='Carrie') return "15";
-    if (using==='SMARTS') return "You do not have an account.";
+    if (using==='SMARTS') return "33";
     if (using==='[E]') return "12";
     if (using==='Bad Guy') return "You do not have an account.";
     if (using==='Deatheye') return "You do not have an account.";
     if (using==='MortalTripod') return "123";
     if (using==='SpeedyBek') return "11";
     if (using==='SparklePony') return "231";
+    if (using==='Cookies by Candlelight - Business') return '5';
+    if (using==='The Golden Silohoutte - Business') return '0';
+    return "Error getting craften";
+}
+
+function getStartingCraften() {
+    if (using==='YBG') return 83;
+    if (using==='Cap') return 1;
+    if (using==='Carrie') return 15;
+    if (using==='SMARTS') return 33;
+    if (using==='[E]') return 12;
+    if (using==='Bad Guy') return 0;
+    if (using==='Deatheye') return 0;
+    if (using==='MortalTripod') return 123;
+    if (using==='SpeedyBek') return 11;
+    if (using==='SparklePony') return 231;
     return "Error getting craften";
 }
 function getHashPass() {
     if (using==='YBG') return "0101000001100101011000010110001101101000010000100110111101101101011000100110010101110010";
     if (using==='Cap') return "01101111011011100111100101101111011101010111001001101100011001010110011001110100";
-    if (using==='Carrie') return "15";
-    if (using==='SMARTS') return "You do not have an account.";
+    if (using==='Carrie') return "01100101011011100110011101101100011000010110111001100100";
+    if (using==='SMARTS') return "011010110111001001101001011100110111000001111001";
     if (using==='[E]') return "011001100110110001111001011000100111010101110100011101000110010101110010";
-    if (using==='Bad Guy') return "You do not have an account.";
-    if (using==='Deatheye') return "You do not have an account.";
-    if (using==='MortalTripod') return "123";
-    if (using==='SpeedyBek') return "11";
-    if (using==='SparklePony') return "231";
+    if (using==='Bad Guy') return "01100011011100100110100101101101011010010110111001100001011011000110110001111001";
+    if (using==='Deatheye') return "01110100011010000110010101100010011011110111001101110011";
+    if (using==='MortalTripod') return "0110110101101111011100100111010001101001011011010110010101110010011101000111001001101001";
+    if (using==='SpeedyBek') return "011000100110010101101011011010110110000101101000";
+    if (using==='SparklePony') return "011010010110110001101111011101100110010101110100011010000110010101101101";
     if (using==='Squire') return "Cheater."
     return "Error getting security code.";
 }
@@ -167,16 +205,16 @@ function getHashPass() {
 function getCards() {
     let returned = '';
 
-    if (using==='YBG') returned= getCardItem("Normal Debit Card",'Active');
+    if (using==='YBG') returned= getCardItem("Normal Debit Card",'Active')+getCardItem("Normal Credit Card",'Active');
     if (using==='Cap') returned= "- You have no active cards.";
     if (using==='Carrie') returned= "- You have no active cards.";
-    if (using==='SMARTS') returned= "You do not have an account.";
+    if (using==='SMARTS') returned= "- You have no active cards.";
     if (using==='[E]') returned= "- You have no active cards.";
     if (using==='Bad Guy') returned= "You do not have an account.";
     if (using==='Deatheye') returned= "You do not have an account.";
     if (using==='MortalTripod') returned= getCardItem("Normal Debit Card",'Active');
     if (using==='SpeedyBek') returned= "- You have no active cards.";
-    if (using==='SparklePony') returned= "- You have no active cards.";
+    if (using==='SparklePony') returned= getCardItem("Normal Debit Card",'Active')+getCardItem("Business Debit Card",'Active');
     if (using==='Squire') returned= getCardItem("Normal Debit Card",'Active');
     if (returned!=="You do not have an account.") {
         returned+='<br> - <a class="green" onclick="loadNewCards()">Order New Cards</a>';
@@ -200,6 +238,10 @@ function getCardButton(cardname) {
     return `<a href="${getEmailLink('Requesting New Card, '+using+', '+removeAfterBrTag(cardname)+'','This is an automated request for a '+removeAfterBrTag(cardname)+'')}"><button class="wider">${cardname}</button></a>`;
 }
 
+function getPersonButton(cardname) {
+    return `<a href="${getEmailLink('Craften transfer request: '+using+ ' to ' + cardname, 'This a request for a transfer of AMOUNTHERE.')}"><button class="wider">${cardname}</button></a>`;
+}
+
 function getCardFailButton(cardname) {
     return `<a><button class="widerred">Insufficient T Score - ${cardname}</button></a>`;
 }
@@ -212,6 +254,42 @@ function removeAfterBrTag(inputString) {
     return inputString;
 }
 
+function getPurchases() {
+    if (using==='YBG') return [new Purchase(-getStartingCraften(), 'Starting Balance'), new Purchase(50,'SQTech Mail Order'),new Purchase(-58,'In Person Deposit')];
+    if (using==='Cap') return null;
+    if (using==='Carrie') return null;
+    if (using==='SMARTS') return null;
+    if (using==='[E]') return null;
+    if (using==='Bad Guy') return null;
+    if (using==='Deatheye') return null;
+    if (using==='MortalTripod') return null;
+    if (using==='SpeedyBek') return null;
+    if (using==='SparklePony') return [new Purchase(-231, 'Starting Balance'), new Purchase(128, 'SQTech Mail Order'), new Purchase(-10,'SQTech Inc'),new Purchase(-20,'SQTech Inc'),new Purchase(5,'SQTech Inc'),new Purchase(5,'TRANSFER TO "Cookies by Candlelight"')];
+    if (using==='Squire') return "Cheater."
+    return "Error getting security code.";
+}
+
+function loadTransferPage() {
+    let finito = `<h1>Select account, then fill out amount in email.</h1> <div class="topcorner" class="logout"><button class="btn" onclick="loadBankPage()">Back</button></div>
+    <hr>`;
+
+    let accs = ["YBG",'Cap',"Carrie","SMARTS",'[E]','MortalTripod','SpeedyBek',"SparklePony",'Squire','The Golden Silhouette - Business','Cookies by Candlelight - Business'];
+
+
+    for (let i = 0; i<accs.length; i++) {
+        if (using==='YBG') transferPerson = 'Cap';
+        if (accs[i]!==using) {
+            let normcred = getPersonButton(accs[i]);
+            finito+='<br><br>'+normcred;
+        }
+    }
+    //finito+=`<br><br><h2 id="person">Person to transfer to: ${transferPerson}.</h3><br>`;
+    document.body.innerHTML=finito;
+}
+
+function getTransferAmount() {
+    return document.getElementById('quantity').innerHTML;
+}
 
 function loadNewCards() {
     let normdeb = getCardButton('Normal Debit Card');
@@ -234,4 +312,26 @@ function loadNewCards() {
         finito+='<br><br>'+normcred;
     }
     document.body.innerHTML=finito;
+}
+
+class Purchase {
+    
+    constructor(value, party) {
+        this.party=party;
+        this.value=value;
+        if (value<=0) {
+            this.value=Math.abs(this.value);
+            this.dep = true;
+        } else this.dep=false;
+    }
+    getHtmlRep(amount) {
+        if (this.dep) {
+            return `<h3 class="good">- ${this.party}; +${this.value}; ${amount} Remaining</h3>`;
+        } else return `<h3 class="bad">- ${this.party}; -${this.value}; ${amount} Remaining</h3>`;
+    }
+
+    getValue() {
+        return this.value;
+    }
+
 }
